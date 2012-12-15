a78f7ab043d3720a4b8f517c555aa25ce19181cf
Xaxis <william.neeley@gmail.com>
2012-12-15 15:14:23 -0800

    Added inclusion and exclusion function call lists. Added additional 'exec' method request functionality.

--- lib/func_request.php
+++ lib/func_request.php
@@ -1,12 +1,23 @@
 <?php
 /*
- * For security reasons it is advisable to dissalow calls
- * to certain functions, ESPECIALLY when client side 
- * requests determine which function are called. Thus, we
- * impliment a disabled function array that is referenced
- * everytime a user makes a function request.
+ * While the jqueryphp plugin allows for the execution of 
+ * arbitrary code via the 'exec' method, this functionality
+ * is disabled by default. The plugin request 'exec' uses
+ * the eval() construct which is very dangerous. If it is 
+ * used pay special attention not to pass any user provided
+ * data without proper validation in place.
  */
-$disabled_functions = array(
+$config = array(
+	'EXEC' 			=> false,
+	'EXCLUSIVE'		=> true
+);
+
+/*
+ * The exclusion_list array is used when the EXCLUSIVE configuration
+ * variable is set to true. In such a case all PHP functions are 
+ * allowed except those found in the array.
+ */
+$exclusion_list = array(
 	'`',
 	'create_function',
 	'escapeshellcmd',
@@ -24,36 +35,96 @@ $disabled_functions = array(
 );
 
 /*
- * We receive the function requested and arguments that
- * are to be passed to it.
+ * The inclusion_list array is used when the EXCLUSIVE configuration
+ * variable is set to false. In such a case all PHP functions are
+ * disallowed except for those found in the array.
  */
-$func_request = $_POST['func'];
-$func_args = $_POST['args'];
+$inclusion_list = array(
+	// 'strlen',			// (e.g. Allowing the strlen function)
+	// 'highlight_string'	// (e.g. Etc...)
+);
 
 /*
- * Next we test for the functions existence. NOTE! Remember
- * to impliment a function policy list to specifically 
- * dissallow dangerous PHP functions.
+ * The first data passed from the client is which method request
+ * is being made. For instance are they making a 'call' to a PHP
+ * function or are they attempting to run PHP code written in 
+ * JavaScript via 'exec'.
  */
-if ( function_exists($func_request) && !in_array($func_request, $disabled_functions) ) {
-	$function = $func_request;
-} else {
-	$function = false;
-}
+$method_request = $_POST['method'] ? $_POST['method'] : false;
+if ( $method_request ) {
 
-/*
- * Next we take our $func_args which should contain a JSON
- * encoded string and convert it into a PHP associative array.
- */
-$args_arr = json_decode($func_args, false);
- 
-/*
- * If the user requested function exists and is allowed
- * we proceed to call that function, passing any arguments
- * given with the requested function.
- */
-if ( $function !== false ) {
-	$call = $function;
-	echo call_user_func_array($call, $args_arr);
+	switch ( $method_request ) {
+		
+		/*
+		 * This method handles all calls to pre-existing PHP functions
+		 * regardless of whether they are native or user defined.
+		 */
+		case 'call' :
+			/*
+			 * We receive the function requested and arguments that
+			 * are to be passed to it.
+			 */
+			$func_request = $_POST['func'] ? $_POST['func'] : false;
+			$func_args = $_POST['args'] ? $_POST['args'] : false;
+			
+			/*
+			 * Based on the EXCLUSIVE configuration variable we attempt to
+			 * build our function call.
+			 */
+			switch ( $config['EXCLUSIVE'] ) {
+				case true :
+					if ( function_exists($func_request) 
+							&& !in_array($func_request, $exclusion_list) ) {
+						$function = $func_request;
+					} else {
+						$function = false;
+					}
+					break;
+					
+				case false :
+					if ( function_exists($func_request) 
+							&& in_array($func_request, $inclusion_list) ) {
+						$function = $func_request;
+					} else {
+						$function = false;
+					}
+					break;
+			}
+			
+			/*
+			 * Next we take our $func_args which should contain a JSON
+			 * encoded string and convert it into a PHP associative array.
+			 */
+			$args_arr = json_decode($func_args, false);
+			 
+			/*
+			 * If the user requested function exists and is allowed
+			 * we proceed to call that function, passing any arguments
+			 * given with the requested function.
+			 */
+			if ( $function !== false ) {
+				$call = $function;
+				echo call_user_func_array($call, $args_arr);
+			}
+			break;
+		
+		/*
+		 * This method handles the execution of user passed PHP strings
+		 * to the server.
+		 */	
+		case 'exec' :
+			if ( $config['EXEC'] === true ) {
+				/*
+				 * We receive the code to be executed from the user.
+				 */
+				 $code_string = $_POST['code'] ? $_POST['code'] : false;
+				 
+				 echo eval( $code_string );
+			} else {
+				echo "Usage of the 'exec' method has been disabled.";
+			}
+			break;
+	}
+
 }
 ?>
