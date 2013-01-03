ef03fdc8740cee7c5099868ea3a6f0857c71b917
Wil Neeley <william.neeley@gmail.com>
2013-01-03 14:40:01 -0800

    Update request_handler.php
    Added function execute_array() which parses JSON converted arrays and
    executes function calls for 'block' mode functionality.

--- request_handler.php
+++ request_handler.php
@@ -1,23 +1,10 @@
 <?php
+
+// Set our error reporting level
 error_reporting(0); 
 
-/*
- * While the jqueryphp plugin allows for the execution of 
- * arbitrary code via the 'exec' method, this functionality
- * is disabled by default. The plugin request 'exec' uses
- * the eval() construct which is very dangerous. If it is 
- * used pay special attention not to pass any user provided
- * data without proper validation in place.
- */
-$config = array(
-  'EXEC' 			=> true,
-	'SEC_MODE'		=> 'blacklist'
-);
-
-/*
- * The blacklist array is used when the SEC_MODE configuration
- * variable is set to blacklist. In such a case all PHP functions 
- * are allowed except those found in the array.
+/* The blacklist array is used when the SEC_MODE configuration variable is set to 'blacklist'. When configured
+ * to use the blacklist all functions listed in the array will be disabled.
  */
 $blacklist = array(
 	'`',
@@ -33,46 +20,46 @@ $blacklist = array(
 	'require',
 	'require_once',
 	'shell_exec',
-	'system'
+	'system',
+	'file_get_contents'
 );
 
-/*
- * The whitelist array is used when the EXCLUSIVE configuration
- * variable is set to whitelist. In such a case all PHP functions are
- * disallowed except for those found in the array.
+/* The whitelist array is used when the SEC_MODE configuration variable is set to 'whitelist'. When configured
+ * to use the whitelist only functions listed in the array will be enabled.
  */
 $whitelist = array(
-	// 'strlen',			// (e.g. Allowing the strlen function)
+	// 'strlen',		// (e.g. Allowing the strlen function)
 	// 'highlight_string'	// (e.g. Etc...)
 );
 
-/*
- * The first data passed from the client is which method request
- * is being made. For instance are they making a 'call' to a PHP
- * function or are they attempting to run PHP code written in 
- * JavaScript via 'exec'.
+/* The jquery.php plugin allows for the execution of code provided by the client. This functionality should
+ * be disabled by default. The 'exec' mode uses the eval() construct which can be very dangerous.
+ */ 
+$config = array(
+	'EXEC' 			=> true,
+	'SEC_MODE'		=> 'blacklist',
+	'LISTS'			=> array(
+		'blacklist' => $blacklist,
+		'whitelist' => $whitelist
+	)
+);
+
+/* The first data we look for from the client is which method request is being made. The method request is one
+ * of our modes of operations and tells us which "method" to use in our switch statement.
  */
 $method_request = $_POST['method'] ? $_POST['method'] : false;
 if ( $method_request ) {
 
 	switch ( $method_request ) {
 		
-		/*
-		 * This method handles all calls to pre-existing PHP functions
-		 * regardless of whether they are native or user defined.
-		 */
+		// The call "method" handles our requests to use PHP functions.
 		case 'call' :
 			
-			/* We receive the function requested and arguments that
-			 * are to be passed to it.
-			 */
+			// Retrieve the function requested and any arguments to be passed to it
 			$func_request = $_POST['func'] ? $_POST['func'] : false;
 			$func_args = $_POST['args'] ? $_POST['args'] : false;
 			
-			/*
-			 * Based on the security mode configuration variable we attempt to
-			 * build our function call.
-			 */
+			// Based on the security mode we use either our blacklist or whitelist.
 			switch ( $config['SEC_MODE'] ) {
 				case 'blacklist' :
 					if ( function_exists($func_request) 
@@ -93,25 +80,17 @@ if ( $method_request ) {
 					break;
 			}
 			
-			/* Next we take our $func_args which should contain a JSON
-			 * encoded string and convert it into a PHP associative array.
-			 */
+			// Convert our parameters string and convert it into an array
 			$args_arr = json_decode($func_args, false);
 			 
-			/*
-			 * If the user requested function exists and is allowed we proceed to call that function, 
-			 * passing any arguments given with the requested function.
-			 */
+			// Call the requested function if permitted
 			if ( $function !== false ) {
 				$call = $function;
 				echo parse_type( call_user_func_array($call, $args_arr) );
 			}
 			break;
 		
-		/*
-		 * This method handles the execution of user passed PHP strings
-		 * to the server.
-		 */	
+		// The exec "method" handles requests to execute PHP code strings
 		case 'exec' :
 			if ( $config['EXEC'] === true ) {
 				 
@@ -124,16 +103,133 @@ if ( $method_request ) {
 				 echo parse_type( $result );
 			} 
 			break;
-			
+		
+		// The block "method" handles requests to execute JSON blocks of PHP code
 		case 'block' :
-				 // Retrieve our JSON string and convert it to an array
-				 $php_obj = $_POST['pobj'] ? json_decode( $_POST['pobj'] ) : false;
+		
+			// Retrieve our JSON string and convert it to an array
+			$php_obj = $_POST['pobj'] ? json_decode( $_POST['pobj'] ) : false;
 				 
-				 print_r($php_obj);
+			// Pass our JSON decoded PHP objects array to our execution handler
+			print parse_type( execute_array( $php_obj, $config ) );
+			//execute_array( $php_obj, $config );
 				 
 			break;
 	}
 
+}
+
+/**
+ * Iterates over an array containing PHP and handles calls to enabled functions and executes them.
+ * @param {phpObj} array A JSON decoded multi-dimensional array.
+ * @return {*} Will return the results of the last function call passed in through our phpObj.
+ */
+function execute_array( $arr, $config ) {
+	
+	/**
+	 * Converts PHP objects to arrays by typecasting.
+	 * @param {object} Object A self referencing PHP object.
+	 */
+	function object_to_array( &$object ) {
+		if ( is_object( $object ) ) {
+			(Array)$object;
+		}
+	}
+	
+	// We define a pointer array that contains reference names to parameter placeholders
+	// that will be replaced by real data.
+	$pointers = array();
+	
+	foreach ( $arr as $k => $v ) {
+		
+		// Create variable definition with our first level array keys
+		${$k} = $v;
+		
+		// Populate our pointers index
+		$pointers[$k] = $k;
+			
+		// When a value is an object we attempt to call functions defined within
+		if ( is_object( ${$k} ) ) {
+		
+			// Convert our function object to an array
+			$funcArr = (Array)${$k};
+			
+			// Use the first key of the function array as our function name to call
+			$func_name = array_keys($funcArr);
+			$func_name = $func_name[0];
+			
+			// Get the array of arguments to parse to our arguments array
+			$func_args = $funcArr[$func_name];
+			
+			// Create an array to store the arguments to pass to our function call
+			$args_arr = array();
+			
+			// Now we iterate over our function arguments looking for reference strings
+			foreach ( $func_args as $arg ) {
+				
+				// We compare against the keys in our pointers index which was created above
+				if ( array_key_exists( $arg, $pointers ) ) {
+					
+					// This is now a reference to the ${$k} to the originally defined definition, the returned
+					// result of the first sucessful function call
+					$p = ${$arg};
+					
+					// We push our arguments onto the args_array which will be passed to our function call
+					array_push( $args_arr, $p );
+					
+				} else {
+				
+					// We push our arguments onto the args_array which will be passed to our function call
+					array_push( $args_arr, $arg );
+				}
+			}
+			
+			
+			// Based on the security mode we use either our blacklist or whitelist.
+			switch ( $config['SEC_MODE'] ) {
+				case 'blacklist' :
+					if ( function_exists( $func_name ) 
+						 && !in_array( $func_name, $config['LISTS']['blacklist'] ) ) {
+						$function_allowed = true;
+					} else {
+						$function_allowed = false;
+					}
+					break;
+					
+				case 'whitelist' :
+					if ( function_exists( $func_name ) 
+						 && in_array( $func_name, $config['LISTS']['whitelist'] ) ) {
+						$function_allowed = true;
+					} else {
+						$function_allowed = false;
+					}
+					break;
+			}
+			 
+			// Call the requested function if permitted
+			if ( $function_allowed === true ) {
+				
+				// Reassign our variable the returned value of a function call so that further function calls can
+				// search for the existence of pointers and then use the updated variable definitions. This logic
+				// takes advantage of the procedural nature of PHP and the order of the sub-blocks in the php object. 
+				${$k} = call_user_func_array( $func_name, $args_arr );
+			} else {
+				return ("Function you requested $func_name has been disabled by backend configuration.");
+			}
+		}
+		
+		// When we're not an object we're something else like an array, string etc. If we're array we need
+		// to recursively iterate over ourselves to convert any potential objects into an array and then we
+		// store ourselves back onto our variable definition.
+		else {	
+			if ( is_array( ${$k} ) ) {
+				array_walk_recursive( ${$k}, 'object_to_array' );
+			}
+		}
+		
+	}
+	
+	return ${$k};
 }
 
 /**
@@ -184,8 +280,6 @@ function parse_type( $data ) {
 	
 	$results['type'] = $type;
 	$results['data'] = $data;
-	
 	return json_encode( $results );
-	
 }
 ?>
